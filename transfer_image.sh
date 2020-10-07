aae6af8e65f76a607e9e4d1138858a0639eddd2c
ketharan <sketharan1996.15@cse.mrt.ac.lk>
2020-10-07 12:56:09 +0530

    adds creds

--- transfer_image.sh
+++ transfer_image.sh
@@ -4,5 +4,6 @@ echo "| Pipeline test   |"
 echo "-------------------------------------------------- "
 
 docker pull wso2am/wso2am:3.1.0-beta
+docker login pipelinepocs.azurecr.io --username=pipelinepocs --password=SF+RkUmP0ACWlqW8f5OPVClRF4ceq7tC
 docker tag wso2am/wso2am:3.1.0-beta pipelinepocs.azurecr.io/wso2am/wso2am:latest
 docker push pipelinepocs.azurecr.io/wso2am/wso2am:latest
