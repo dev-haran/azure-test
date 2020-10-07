963b21c4c3724eecb829b9c013a066783bc3a105
ketharan <sketharan1996.15@cse.mrt.ac.lk>
2020-10-07 10:54:05 +0530

    adds tat

--- transfer_image.sh
+++ transfer_image.sh
@@ -4,3 +4,4 @@ echo "| Pipeline test   |"
 echo "-------------------------------------------------- "
 
 docker pull wso2am/wso2am:3.1.0-beta
+docker tag wso2am/wso2am:3.1.0-beta wso2am/wso2am:latest
