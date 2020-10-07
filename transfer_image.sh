2d4b21d1a27ca432d30ae8843d835962b0940e7b
ketharan <sketharan1996.15@cse.mrt.ac.lk>
2020-10-07 12:27:35 +0530

    push with script

--- transfer_image.sh
+++ transfer_image.sh
@@ -5,3 +5,4 @@ echo "-------------------------------------------------- "
 
 docker pull wso2am/wso2am:3.1.0-beta
 docker tag wso2am/wso2am:3.1.0-beta pipeline_acr/wso2am/wso2am:latest
+docker push wso2am/wso2am:3.1.0-beta
